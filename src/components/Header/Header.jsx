2624c0056ab46e80ca60f59a8d01900c5c6c0661
SujSelvam <suja.panneerselvams@gmail.com>
2024-07-24 12:12:15 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Beloved Dishes Here</h2>
+        <p>Explore a diverse menu with mouthwatering options. Our goal is to indulge your taste buds and enhance your dining experience, one scrumptious dish at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
